0b2da792b8801e7c686d5ff18b0a854083c78231
cairesana <caires.ana@gmail.com>
2018-07-07 19:58:21 +0200

    fixed file controlled extention

---
+++ src/controller.ts
@@ -0,0 +1,14 @@
+//testing
+import {Controller, Get} from 'routing-controllers'
+
+@Controller()
+export default class MainController {
+
+    @Get("/hello")
+    main() {
+       return {
+         hello: 'World'
+       }
+    }
+
+}
