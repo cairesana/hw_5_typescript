2eff29ddb813c2be73434f9ca81a796564b5ec0d
cairesana <caires.ana@gmail.com>
2018-07-07 19:44:33 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// src/index.ts -- test
+import 'reflect-metadata'
+import {createKoaServer} from "routing-controllers"
+import Controller from "./controller"
+import setupDb from './db'
+
+const app = createKoaServer({
+   controllers: [Controller]
+})
+
+setupDb()
+  .then(_ =>
+    app.listen(4000, () => console.log('Listening on port 4000'))
+  )
+  .catch(err => console.error(err))
+
